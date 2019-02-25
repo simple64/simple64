0b53ca3f43e44f793a5efb77520a2a87971cd596
Logan McNaughton <logan@bacoosta.com>
2019-02-25 08:02:52 -0700

    clean clean

--- clean.sh
+++ clean.sh
@@ -8,9 +8,6 @@ make clean
 cd $base_dir/mupen64plus-rsp-hle/projects/unix
 make clean
 
-cd $base_dir/mupen64plus-rsp-cxd4/projects/unix
-make clean
-
 cd $base_dir/mupen64plus-input-sdl/projects/unix
 make clean
 
@@ -21,8 +18,6 @@ rm -rf $base_dir/mupen64plus-gui/build
 
 cd $base_dir/GLideN64/projects/cmake
 make clean
-
-rm -rf $base_dir/angrylion-rdp-plus/build
 
 cd $base_dir
 rm -rf $base_dir/mupen64plus
