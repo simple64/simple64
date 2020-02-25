ea24fd756b633221f1568ac29d5a40d4c57caf68
Logan McNaughton <logan@bacoosta.com>
2020-02-25 09:44:22 -0700

    Re-add build script

---
+++ build.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+UNAME=$(uname -s)
+if [[ $UNAME == *"MINGW"* ]]; then
+  suffix=".dll"
+  if [[ $UNAME == *"MINGW64"* ]]; then
+    mingw_prefix="mingw64"
+  else
+    mingw_prefix="mingw32"
+  fi
+else
+  suffix=".so"
+fi
+
+export NO_ASM=1
+
+install_dir=$PWD/mupen64plus
+mkdir $install_dir
+base_dir=$PWD
+
+cd $base_dir/mupen64plus-core/projects/unix
+make -j4 all
+cp -P $base_dir/mupen64plus-core/projects/unix/*$suffix* $install_dir
+cp $base_dir/mupen64plus-core/data/* $install_dir
+
+cd $base_dir/mupen64plus-rsp-hle/projects/unix
+make -j4 all
+cp $base_dir/mupen64plus-rsp-hle/projects/unix/*$suffix $install_dir
+
+cd $base_dir/mupen64plus-input-sdl/projects/unix
+make -j4 all
+cp $base_dir/mupen64plus-input-sdl/projects/unix/*$suffix $install_dir
+cp $base_dir/mupen64plus-input-sdl/data/* $install_dir
+
+cd $base_dir/mupen64plus-audio-sdl2/projects/unix
+make -j4 all
+cp $base_dir/mupen64plus-audio-sdl2/projects/unix/*$suffix $install_dir
+
+mkdir -p $base_dir/mupen64plus-gui/build
+cd $base_dir/mupen64plus-gui/build
+if [[ $UNAME == *"MINGW"* ]]; then
+  qmake ../mupen64plus-gui.pro
+  make -j4
+  cp $base_dir/mupen64plus-gui/build/mupen64plus-gui.exe $install_dir
+else
+  qmake ../mupen64plus-gui.pro
+  make -j4
+  cp $base_dir/mupen64plus-gui/build/mupen64plus-gui $install_dir
+fi
+
+cd $base_dir/GLideN64/src
+./getRevision.sh
+
+cd $base_dir/GLideN64/src/GLideNUI
+mkdir build
+cd build
+qmake ../GLideNUI.pro
+make -j4
+
+cd $base_dir/GLideN64/projects/cmake
+if [[ $UNAME == *"MINGW"* ]]; then
+  cmake -G "MSYS Makefiles" -DVEC4_OPT=On -DCRC_OPT=On -DMUPENPLUSAPI=On ../../src/
+else
+  rm -rf ../../src/GLideNHQ/inc
+  cmake -DUSE_SYSTEM_LIBS=On -DVEC4_OPT=On -DCRC_OPT=On -DMUPENPLUSAPI=On ../../src/
+fi
+make -j4
+
+if [[ $UNAME == *"MINGW"* ]]; then
+  cp mupen64plus-video-GLideN64$suffix $install_dir
+else
+  cp plugin/Release/mupen64plus-video-GLideN64$suffix $install_dir
+fi
+cp $base_dir/GLideN64/ini/GLideN64.custom.ini $install_dir
+
+if [[ $UNAME == *"MINGW"* ]]; then
+  cd $install_dir
+  windeployqt.exe mupen64plus-gui.exe
+
+  if [[ $UNAME == *"MINGW64"* ]]; then
+    my_os=win64
+    cp /$mingw_prefix/bin/libgcc_s_seh-1.dll $install_dir
+  else
+    my_os=win32
+    cp /$mingw_prefix/bin/libgcc_s_dw2-1.dll $install_dir
+  fi
+  cp /$mingw_prefix/bin/libwinpthread-1.dll $install_dir
+  cp /$mingw_prefix/bin/SDL2.dll $install_dir
+  cp /$mingw_prefix/bin/libpng16-16.dll $install_dir
+  cp /$mingw_prefix/bin/libglib-2.0-0.dll $install_dir
+  cp /$mingw_prefix/bin/libstdc++-6.dll $install_dir
+  cp /$mingw_prefix/bin/zlib1.dll $install_dir
+  cp /$mingw_prefix/bin/libintl-8.dll $install_dir
+  cp /$mingw_prefix/bin/libpcre-1.dll $install_dir
+  cp /$mingw_prefix/bin/libiconv-2.dll $install_dir
+  cp /$mingw_prefix/bin/libharfbuzz-0.dll $install_dir
+  cp /$mingw_prefix/bin/libgraphite2.dll $install_dir
+  cp /$mingw_prefix/bin/libfreetype-6.dll $install_dir
+  cp /$mingw_prefix/bin/libbz2-1.dll $install_dir
+  cp /$mingw_prefix/bin/libminizip-1.dll $install_dir
+  cp /$mingw_prefix/bin/libsamplerate-0.dll $install_dir
+  cp /$mingw_prefix/bin/libjasper-4.dll $install_dir
+  cp /$mingw_prefix/bin/libjpeg-8.dll $install_dir
+  cp /$mingw_prefix/bin/libpcre2-16-0.dll $install_dir
+  cp /$mingw_prefix/bin/libdouble-conversion.dll $install_dir
+  cp /$mingw_prefix/bin/libicuin65.dll $install_dir
+  cp /$mingw_prefix/bin/libicuuc65.dll $install_dir
+  cp /$mingw_prefix/bin/libicudt65.dll $install_dir
+  cp /$mingw_prefix/bin/libzstd.dll $install_dir
+  cp $base_dir/7za.exe $install_dir
+else
+  if [[ $HOST_CPU == "i686" ]]; then
+    my_os=linux32
+  else
+    my_os=linux64
+  fi
+fi
+
+cd $base_dir
+if [[ $1 == "aws" ]]; then
+  rm $base_dir/*.zip
+  DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+  HASH=$(git rev-parse --short HEAD)
+  zip -r m64p-$my_os-$HASH.zip mupen64plus
+  aws s3 cp m64p-*.zip s3://m64p/m64p/ --quiet
+fi
