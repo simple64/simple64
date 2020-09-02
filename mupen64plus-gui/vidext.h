102d939d2b81802005740519efc67b86ddafc243
Logan McNaughton <logan@bacoosta.com>
2020-09-02 16:37:35 -0700

    update gui

--- mupen64plus-gui/vidext.h
+++ mupen64plus-gui/vidext.h
@@ -14,7 +14,7 @@ m64p_error qtVidExtFuncQuit(void);
 m64p_error qtVidExtFuncListModes(m64p_2d_size *SizeArray, int *NumSizes);
 m64p_error qtVidExtFuncListRates(m64p_2d_size Size, int *NumRates, int *Rates);
 m64p_error qtVidExtFuncSetMode(int Width, int Height, int, int, int);
-m64p_error qtVidExtFuncSetModeWithRate(int Width, int Height, int RefreshRate, int BitsPerPixel, m64p_video_mode ScreenMode, m64p_video_flags Flags);
+m64p_error qtVidExtFuncSetModeWithRate(int Width, int Height, int RefreshRate, int BitsPerPixel, int ScreenMode, int Flags);
 m64p_function     qtVidExtFuncGLGetProc(const char*Proc);
 m64p_error qtVidExtFuncGLSetAttr(m64p_GLattr Attr, int Value);
 m64p_error qtVidExtFuncGLGetAttr(m64p_GLattr Attr, int *pValue);
