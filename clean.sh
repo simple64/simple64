557b13b1792b105db5441a37465082049ec945fb
Joel Denning <joeldenning@gmail.com>
2020-10-24 10:17:06 -0600

    Self review

--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 rm -rf mupen64plus mupen64plus-audio-sdl2/projects/unix/_obj mupen64plus-audio-sdl2/projects/unix/mupen64plus-audio-sdl2.dylib mupen64plus-core/projects/unix/_obj/ mupen64plus-core/projects/unix/libmupen64plus.dylib mupen64plus-gui/build mupen64plus-input-qt/build mupen64plus-input-raphnetraw/projects/unix/_obj/ mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.dylib mupen64plus-rsp-hle/projects/unix/_obj/ mupen64plus-rsp-hle/projects/unix/mupen64plus-rsp-hle.dylib GLideN64/projects/cmake/CMakeFiles GLideN64/projects/cmake/mupen64plus-video-GLideN64_autogen GLideN64/projects/cmake/osal GLideN64/projects/cmake/cmake_install.cmake GLideN64/projects/cmake/CMakeCache.txt GLideN64/projects/cmake/Makefile
