277f5752e1589c7060cfe30eddb34ca21eb84dce
Logan McNaughton <logan@bacoosta.com>
2020-10-29 07:30:41 -0700

    update gui

--- mupen64plus-gui/netplay/waitroom.cpp
+++ mupen64plus-gui/netplay/waitroom.cpp
@@ -13,6 +13,7 @@ WaitRoom::WaitRoom(QString filename, QJsonObject room, QWebSocket *socket, QWidg
     room_port = room.value("port").toInt();
     room_name = room.value("room_name").toString();
     file_name = filename;
+    started = 0;
 
     w->getSettings()->setValue("netplay_name", player_name);
 
@@ -65,18 +66,23 @@ WaitRoom::WaitRoom(QString filename, QJsonObject room, QWebSocket *socket, QWidg
     connect(chatEdit, &QLineEdit::returnPressed, this, &WaitRoom::sendChat);
     layout->addWidget(chatEdit, 10, 0, 1, 2);
 
+    discordCheckbox = new QCheckBox("Join Discord Voice Chat (must have Discord App running)", this);
+    discordCheckbox->setEnabled(false);
+    layout->addWidget(discordCheckbox, 11, 0, 1, 2);
+    connect(discordCheckbox, &QCheckBox::stateChanged, this, &WaitRoom::discordCheck);
+
     startGameButton = new QPushButton(this);
     startGameButton->setText("Start Game");
     startGameButton->setAutoDefault(0);
     connect(startGameButton, &QPushButton::released, this, &WaitRoom::startGame);
-    layout->addWidget(startGameButton, 11, 0, 1, 2);
+    layout->addWidget(startGameButton, 12, 0, 1, 2);
 
     motd = new QLabel(this);
     motd->setTextFormat(Qt::RichText);
     motd->setTextInteractionFlags(Qt::TextBrowserInteraction);
     motd->setOpenExternalLinks(true);
     motd->setAlignment(Qt::AlignCenter);
-    layout->addWidget(motd, 12, 0, 1, 2);
+    layout->addWidget(motd, 13, 0, 1, 2);
 
     setLayout(layout);
 
@@ -91,6 +97,40 @@ WaitRoom::WaitRoom(QString filename, QJsonObject room, QWebSocket *socket, QWidg
     timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, &WaitRoom::sendPing);
     timer->start(5000);
+
+    struct DiscordActivity activity;
+    struct DiscordActivityAssets assets;
+    memset(&activity, 0, sizeof(activity));
+    memset(&assets, 0, sizeof(assets));
+    strcpy(assets.large_image, "6205049");
+    strcpy(assets.large_text, "https://m64p.github.io");
+    activity.assets = assets;
+    strncpy(activity.details, room.value("game_name").toString().toLatin1().data(), 128);
+    strcpy(activity.state, "Netplay waiting room");
+    w->updateDiscordActivity(activity);
+}
+
+static void discordConnectCallback(void*, enum EDiscordResult result, struct DiscordLobby* lobby)
+{
+    if (result == DiscordResult_Ok)
+        w->getDiscordApp()->lobbies->connect_voice(w->getDiscordApp()->lobbies, lobby->id, w->getDiscordApp(), nullptr);
+}
+
+static void discordDisconnectCallback(void* _discord_id, enum EDiscordResult)
+{
+    QString* id = (QString*) _discord_id;
+    w->getDiscordApp()->lobbies->disconnect_lobby(w->getDiscordApp()->lobbies, id->toLongLong(), w->getDiscordApp(), nullptr);
+}
+
+void WaitRoom::discordCheck(int state)
+{
+    if (!w->getDiscordApp()->lobbies)
+        return;
+
+    if (state == Qt::Checked)
+        w->getDiscordApp()->lobbies->connect_lobby(w->getDiscordApp()->lobbies, discord_id.toLongLong(), discord_secret.toLatin1().data(), w->getDiscordApp(), discordConnectCallback);
+    else if (state == Qt::Unchecked)
+        w->getDiscordApp()->lobbies->disconnect_voice(w->getDiscordApp()->lobbies, discord_id.toLongLong(), &discord_id, discordDisconnectCallback);
 }
 
 void WaitRoom::sendPing()
@@ -103,7 +143,14 @@ void WaitRoom::sendPing()
         QJsonDocument json_doc = QJsonDocument(json);
         webSocket->sendBinaryMessage(json_doc.toJson());
     }
-
+    if (!discordCheckbox->isEnabled() && w->getDiscordApp()->lobbies)
+    {
+        QJsonObject json;
+        json.insert("type", "get_discord_lobby");
+        json.insert("port", room_port);
+        QJsonDocument json_doc = QJsonDocument(json);
+        webSocket->sendBinaryMessage(json_doc.toJson());
+    }
     webSocket->ping();
 }
 
@@ -148,6 +195,8 @@ void WaitRoom::sendChat()
 
 void WaitRoom::onFinished(int)
 {
+    if (!started)
+        w->clearDiscordActivity();
     timer->stop();
     webSocket->close();
     webSocket->deleteLater();
@@ -177,6 +226,7 @@ void WaitRoom::processBinaryMessage(QByteArray message)
     }
     else if (json.value("type").toString() == "begin_game")
     {
+        started = 1;
 #ifndef SINGLE_THREAD
         w->openROM(file_name, webSocket->peerAddress().toString(), room_port, player_number);
 #else
@@ -188,5 +238,11 @@ void WaitRoom::processBinaryMessage(QByteArray message)
     {
         QString message = json.value("message").toString();
         motd->setText(message);
+    }
+    else if (json.value("type").toString() == "discord_lobby")
+    {
+        discord_id = json.value("id").toString();
+        discord_secret = json.value("secret").toString();
+        discordCheckbox->setEnabled(true);
     }
 }
