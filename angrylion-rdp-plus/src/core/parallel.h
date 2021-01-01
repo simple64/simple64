a7edac13592b7a08d93073259348721b91b0bee9
Logan McNaughton <logan@bacoosta.com>
2021-01-01 03:34:14 +0000

    update angrylion

--- angrylion-rdp-plus/src/core/parallel.h
+++ angrylion-rdp-plus/src/core/parallel.h
@@ -5,10 +5,11 @@ extern "C" {
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define PARALLEL_MAX_WORKERS 64u
 
-void parallel_init(uint32_t num);
+void parallel_init(uint32_t num, bool busy);
 void parallel_run(void task(uint32_t));
 uint32_t parallel_num_workers();
 void parallel_close();
