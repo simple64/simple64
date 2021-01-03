1a2f3c43944c42f78cb3f517c5364545945b50ae
Logan McNaughton <logan@bacoosta.com>
2021-01-03 16:52:28 +0000

    update gliden64

--- GLideN64/src/Config.cpp
+++ GLideN64/src/Config.cpp
@@ -45,7 +45,7 @@ void Config::resetToDefaults()
 	generalEmulation.enableDitheringQuantization = 1;
 	generalEmulation.rdramImageDitheringMode = BufferDitheringMode::bdmBlueNoise;
 	generalEmulation.enableHWLighting = 0;
-	generalEmulation.enableCoverage = 1;
+	generalEmulation.enableCoverage = 0;
 	generalEmulation.enableCustomSettings = 1;
 	generalEmulation.enableShadersStorage = 1;
 	generalEmulation.enableLegacyBlending = 0;
