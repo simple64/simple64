fdab03260ecef0964879207cc01bd282b4533a71
Logan McNaughton <logan@bacoosta.com>
2021-01-04 14:46:40 -0700

    update gui

--- mupen64plus-gui/netplay/joinroom.h
+++ mupen64plus-gui/netplay/joinroom.h
@@ -8,6 +8,7 @@
 #include <QWebSocket>
 #include <QLineEdit>
 #include <QPushButton>
+#include <QLabel>
 
 class JoinRoom : public QDialog
 {
@@ -16,14 +17,15 @@ public:
     JoinRoom(QWidget *parent = nullptr);
 private slots:
     void downloadFinished(QNetworkReply *reply);
-    void serverChanged(QString server);
+    void serverChanged(int index);
     void onConnected();
     void processBinaryMessage(QByteArray message);
     void refresh();
     void joinGame();
     void onFinished(int result);
     void processBroadcast();
-    void connectionFailed();
+    void sendPing();
+    void updatePing(quint64 elapsedTime, const QByteArray&);
 private:
     void resetList();
     QComboBox *serverChooser;
@@ -35,6 +37,7 @@ private:
     QPushButton *joinButton;
     QPushButton *refreshButton;
     QLineEdit *inputDelay;
+    QLabel *pingLabel;
     QList<QJsonObject> rooms;
     int row = 0;
     int launched;
