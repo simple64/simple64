cf239f66373d2e3d2d303fea98c34c93cb8c9357
Logan McNaughton <logan@bacoosta.com>
2021-05-31 11:34:59 -0600

    update input qt

--- mupen64plus-input-qt/osal/osal_dynamiclib.h
+++ mupen64plus-input-qt/osal/osal_dynamiclib.h
@@ -28,11 +28,7 @@
 extern "C" {
 #endif
 
-m64p_error osal_dynlib_open(m64p_dynlib_handle *pLibHandle, const char *pccLibraryPath);
-
 void *     osal_dynlib_getproc(m64p_dynlib_handle LibHandle, const char *pccProcedureName);
-
-m64p_error osal_dynlib_close(m64p_dynlib_handle LibHandle);
 
 #ifdef __cplusplus
 }
