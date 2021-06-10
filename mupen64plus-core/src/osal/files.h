6e6542269aca64f6ba27cfe3aea93d4584cb50d4
Logan McNaughton <logan@bacoosta.com>
2021-06-09 21:05:48 -0600

    update core

--- mupen64plus-core/src/osal/files.h
+++ mupen64plus-core/src/osal/files.h
@@ -26,6 +26,8 @@
 #if !defined (OSAL_FILES_H)
 #define OSAL_FILES_H
 
+#include <zlib.h>
+
 /* some file-related preprocessor definitions */
 #if defined(WIN32) && !defined(__MINGW32__)
   #include <io.h> // For _unlink()
@@ -56,6 +58,9 @@ extern const char * osal_get_shared_filepath(const char *filename, const char *f
 extern const char * osal_get_user_configpath(void);
 extern const char * osal_get_user_datapath(void);
 extern const char * osal_get_user_cachepath(void);
+
+extern FILE * osal_file_open (const char *filename, const char *mode);
+extern gzFile osal_gzopen(const char *filename, const char *mode);
 
 #endif /* OSAL_FILES_H */
 
