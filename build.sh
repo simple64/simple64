90ab1dbeb7f82fcf7eb210cd8d0f67329c29970e
Logan McNaughton <logan@bacoosta.com>
2021-06-21 11:10:27 -0600

    asm

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ mkdir -p $install_dir
 base_dir=$PWD
 
 cd $base_dir/mupen64plus-core/projects/unix
-make NETPLAY=1 OSD=0 NO_ASM=1 -j4 all
+make NETPLAY=1 OSD=0 -j4 all
 cp -P $base_dir/mupen64plus-core/projects/unix/*$suffix* $install_dir
 cp $base_dir/mupen64plus-core/data/* $install_dir
 
