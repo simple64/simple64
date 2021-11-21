8fb1e7b514d80467ad7236d2ba731673fd08e9fb
Logan McNaughton <logan@bacoosta.com>
2021-11-21 14:36:06 -0700

    update input qt

---
+++ mupen64plus-input-qt/funcs.h
@@ -0,0 +1,9 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#include "m64p_config.h"
+
+extern ptr_ConfigGetUserConfigPath ConfigGetUserConfigPath;
+extern ptr_ConfigGetUserDataPath ConfigGetUserDataPath;
+
+#endif
