6a937569e8547ac2512b2bbcce95fb72e307504c
Logan <loganmc10@users.noreply.github.com>
2021-12-29 12:35:37 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -105,6 +105,7 @@ if [[ $UNAME == *"MINGW"* ]]; then
     my_os=win32
     cp /$mingw_prefix/bin/libgcc_s_dw2-1.dll $install_dir
   fi
+# WINEDEBUG=+loaddll wine ./mupen64plus-gui.exe 2> out.txt
   cp -v /$mingw_prefix/bin/libwinpthread-1.dll $install_dir
   cp -v /$mingw_prefix/bin/SDL2.dll $install_dir
   cp -v /$mingw_prefix/bin/SDL2_net.dll $install_dir
