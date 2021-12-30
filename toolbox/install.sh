50ced8b5c634de2d683daaf61788bcfb04adaa3b
Logan McNaughton <logan@bacoosta.com>
2021-12-30 08:33:11 -0700

    echo up

--- toolbox/install.sh
+++ toolbox/install.sh
@@ -4,7 +4,8 @@ podman stop m64p
 toolbox rm m64p -y
 podman pull quay.io/loganmc10/m64p:latest
 toolbox create --image quay.io/loganmc10/m64p m64p -y
-podman image prune -f
+toolbox run -c m64p echo UP
+podman image prune -a -f
 wget -q https://raw.githubusercontent.com/loganmc10/m64p/main/snap/gui/m64p.png -O ~/.local/share/icons/m64p.png
 wget -q https://raw.githubusercontent.com/loganmc10/m64p/main/toolbox/m64p.desktop -O ~/.local/share/applications/m64p.desktop
 chmod +x ~/.local/share/applications/m64p.desktop
