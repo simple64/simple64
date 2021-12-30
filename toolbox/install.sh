53a9275ef19c5efcb2bc42745404795c05f6bcf7
Logan <loganmc10@users.noreply.github.com>
2021-12-30 08:46:38 -0700

    Update install.sh

--- toolbox/install.sh
+++ toolbox/install.sh
@@ -5,6 +5,7 @@ toolbox rm m64p -y
 podman pull quay.io/loganmc10/m64p:latest
 toolbox create --image quay.io/loganmc10/m64p m64p -y
 toolbox run -c m64p echo UP
+sleep 5
 podman image prune -a -f
 wget -q https://raw.githubusercontent.com/loganmc10/m64p/main/snap/gui/m64p.png -O ~/.local/share/icons/m64p.png
 wget -q https://raw.githubusercontent.com/loganmc10/m64p/main/toolbox/m64p.desktop -O ~/.local/share/applications/m64p.desktop
