b5307d594eddaeaa3f682bad91cc77f2163e845d
Logan McNaughton <logan@bacoosta.com>
2021-12-30 08:22:18 -0700

    add more

--- toolbox/install.sh
+++ toolbox/install.sh
@@ -7,3 +7,4 @@ toolbox create --image quay.io/loganmc10/m64p m64p -y
 podman image prune -a -f
 wget https://raw.githubusercontent.com/loganmc10/m64p/main/snap/gui/m64p.png -O ~/.local/share/icons/m64p.png
 wget https://raw.githubusercontent.com/loganmc10/m64p/main/toolbox/m64p.desktop -O ~/.local/share/applications/m64p.desktop
+chmod +x ~/.local/share/applications/m64p.desktop
