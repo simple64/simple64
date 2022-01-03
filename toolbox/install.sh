802fc9ba9c569151349b7da5a37dd2331f0ff552
Logan <loganmc10@users.noreply.github.com>
2022-01-03 08:27:22 -0700

    Update install.sh

--- toolbox/install.sh
+++ toolbox/install.sh
@@ -3,7 +3,7 @@
 podman stop m64p
 toolbox rm m64p -y
 podman pull quay.io/loganmc10/m64p:latest
-toolbox create --image quay.io/loganmc10/m64p m64p -y
+toolbox create --image quay.io/loganmc10/m64p:latest m64p -y
 toolbox run -c m64p echo UP
 sleep 5
 podman image prune -a -f
