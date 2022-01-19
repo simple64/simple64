1709cbd1dec268ece4caa5791b17373ba0880b8a
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-01-19 13:19:32 -0700

    update gui

--- mupen64plus-gui/mainwindow.cpp
+++ mupen64plus-gui/mainwindow.cpp
@@ -328,7 +328,11 @@ MainWindow::MainWindow(QWidget *parent) :
     m_title += __DATE__;
     this->setWindowTitle(m_title);
 
-    settings = new QSettings("mupen64plus", "gui", this);
+    QString ini_path = QDir(QCoreApplication::applicationDirPath()).filePath("mupen64plus-gui.ini");
+    settings = new QSettings(ini_path, QSettings::IniFormat, this);
+
+    if (!settings->isWritable())
+        settings = new QSettings("mupen64plus", "gui", this);
 
     if (!settings->contains("version") || settings->value("version").toInt() != SETTINGS_VER)
     {
