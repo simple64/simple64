0cd5169864ded6d81dd7cbc9c765c6faf76e69be
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-02-15 22:21:48 -0700

    zip symlinks

--- build.sh
+++ build.sh
@@ -168,6 +168,6 @@ if [[ "$1" != "nozip" ]]; then
     cd $base_dir
     rm -f $base_dir/*.zip
     HASH=$(git rev-parse --short HEAD)
-    zip -r m64p-$my_os-$HASH.zip mupen64plus
+    zip --symlinks -r m64p-$my_os-$HASH.zip mupen64plus
   fi
 fi
