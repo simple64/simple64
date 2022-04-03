34bb7f42174c7f3157c6ce9b447e0a3ef3137189
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-04-03 10:17:07 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,14 +19,12 @@ else
   suffix=".so"
 fi
 
-export NEW_DYNAREC=1
-
 install_dir=$PWD/mupen64plus
 mkdir -p $install_dir
 base_dir=$PWD
 
 cd $base_dir/mupen64plus-core/projects/unix
-make NETPLAY=1 OSD=0 -j4 all
+make NETPLAY=1 NO_ASM=1 OSD=0 -j4 all
 cp -P $base_dir/mupen64plus-core/projects/unix/*$suffix* $install_dir
 cp $base_dir/mupen64plus-core/data/* $install_dir
 
