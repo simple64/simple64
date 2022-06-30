75d1ae7233e774109cf9cb96db9d47b76033d062
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-06-30 13:46:37 -0600

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-rm -rf mupen64plus mupen64plus-audio-sdl2/projects/unix/_obj mupen64plus-audio-sdl2/projects/unix/mupen64plus-audio-sdl2.dylib mupen64plus-core/projects/unix/_obj/ mupen64plus-core/projects/unix/libmupen64plus.dylib mupen64plus-gui/build mupen64plus-input-qt/build mupen64plus-input-raphnetraw/projects/unix/_obj/ mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.dylib parallel-rsp/build parallel-rdp/build
+rm -rf mupen64plus
+rm -rf mupen64plus-audio-sdl2/projects/unix/_obj
+rm -rf mupen64plus-audio-sdl2/projects/unix/mupen64plus-audio-sdl2.dylib
+rm -rf mupen64plus-core/projects/unix/_obj
+rm -rf mupen64plus-core/projects/unix/libmupen64plus.dylib
+rm -rf mupen64plus-gui/build
+rm -rf mupen64plus-input-qt/build
+rm -rf mupen64plus-input-raphnetraw/projects/unix/_obj
+rm -rf mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.dylib
+rm -rf parallel-rsp/build
+rm -rf parallel-rdp-standalone/build
