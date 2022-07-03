96a6f5c159a4dc70b396a149bb0166b76a5d6a5c
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-07-03 09:28:24 -0600

    update parallel rdp

--- parallel-rdp-standalone/vulkan/context.hpp
+++ parallel-rdp-standalone/vulkan/context.hpp
@@ -135,8 +135,9 @@ struct QueueInfo
 };
 
 class Context
+	: public Util::IntrusivePtrEnabled<Context, std::default_delete<Context>, HandleCounter>
 #ifdef GRANITE_VULKAN_FOSSILIZE
-		: public Fossilize::DeviceQueryInterface
+	, public Fossilize::DeviceQueryInterface
 #endif
 {
 public:
@@ -289,4 +290,6 @@ private:
 	bool descriptor_set_layout_is_supported(const VkDescriptorSetLayoutCreateInfo *set_layout) override;
 #endif
 };
+
+using ContextHandle = Util::IntrusivePtr<Context>;
 }
