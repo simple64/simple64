cc77f5c7eb28f29cce7997c2fe96849664946cc3
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-08-31 11:21:49 -0600

    update name

--- clean.sh
+++ clean.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf mupen64plus
-rm -rf mupen64plus-audio-sdl2/projects/unix/_obj
-rm -rf mupen64plus-audio-sdl2/projects/unix/mupen64plus-audio-sdl2.dylib
+rm -rf simple64
+rm -rf simple64-audio-sdl2/projects/unix/_obj
+rm -rf simple64-audio-sdl2/projects/unix/simple64-audio-sdl2.dylib
 rm -rf mupen64plus-core/projects/unix/_obj
 rm -rf mupen64plus-core/projects/unix/libmupen64plus.dylib
-rm -rf mupen64plus-gui/build
-rm -rf mupen64plus-input-qt/build
+rm -rf simple64-gui/build
+rm -rf simple64-input-qt/build
 rm -rf mupen64plus-input-raphnetraw/projects/unix/_obj
 rm -rf mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.dylib
 rm -rf parallel-rsp/build
