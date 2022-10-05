b999224fec251cf17101341ce63dd3bc20a3c5b4
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-10-05 14:19:31 -0600

    cmake for core

--- clean.sh
+++ clean.sh
@@ -4,8 +4,7 @@ set -e
 
 rm -rf simple64
 rm -rf simple64-audio-sdl2/build
-rm -rf mupen64plus-core/projects/unix/_obj
-rm -rf mupen64plus-core/projects/unix/libmupen64plus.dylib
+rm -rf mupen64plus-core/build
 rm -rf simple64-gui/build
 rm -rf simple64-input-qt/build
 rm -rf simple64-input-raphnetraw/build
