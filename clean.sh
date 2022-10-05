44f2eee29e2ee989efc0fcda27d5e36cff11f7b8
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2022-10-05 13:40:38 -0600

    use cmake (#321)

--- clean.sh
+++ clean.sh
@@ -3,13 +3,11 @@
 set -e
 
 rm -rf simple64
-rm -rf simple64-audio-sdl2/projects/unix/_obj
-rm -rf simple64-audio-sdl2/projects/unix/simple64-audio-sdl2.dylib
+rm -rf simple64-audio-sdl2/build
 rm -rf mupen64plus-core/projects/unix/_obj
 rm -rf mupen64plus-core/projects/unix/libmupen64plus.dylib
 rm -rf simple64-gui/build
 rm -rf simple64-input-qt/build
-rm -rf simple64-input-raphnetraw/projects/unix/_obj
-rm -rf simple64-input-raphnetraw/projects/unix/simple64-input-raphnetraw.dylib
+rm -rf simple64-input-raphnetraw/build
 rm -rf parallel-rsp/build
 rm -rf parallel-rdp-standalone/build
